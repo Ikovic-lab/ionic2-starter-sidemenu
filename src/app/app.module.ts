f85dba0911cf9474da5a899d86da0c59abc695d3
Max Lynch <max@drifty.com>
2016-10-19 16:46:42 -0500

    feat(debugging): use IonicErrorHandler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+
+/**
+ * This is a custom Angular error handler. By default, we report
+ * errors to the IonicDevServer if running, but you can add
+ * other 3rd party error handling services here.
+ */
+class MyErrorHandler implements ErrorHandler {
+  handleError(err:any) : void {
+    IonicErrorHandler.handleError(err);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -19,6 +30,8 @@ import { Page2 } from '../pages/page2/page2';
     Page1,
     Page2
   ],
-  providers: []
+  providers: [
+    { provide: ErrorHandler, useClass: MyErrorHandler }
+  ]
 })
 export class AppModule {}
